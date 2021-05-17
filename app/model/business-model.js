f6c654641d934c452bb17a3ed1cddda12e81f31d
Vivekkarthick2211 <jeevakarthick16@gmail.com>
2021-05-17 17:19:58 +0530

    business

--- app/model/business-model.js
+++ app/model/business-model.js
@@ -4,7 +4,7 @@ const Business =function(){
 }
 
 Business.getAll = result => {
-    sql.query("SELECT * FROM user_primary_info", (err, res) => {
+    sql.query("SELECT business_types FROM businesstypes", (err, res) => {
       if (err) {
         console.log("error: ", err);
         result(null, err);
